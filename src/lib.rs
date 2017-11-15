a7aec216a3e942ba4550d4a907229c789ed3e36f
main() <main@ehvag.de>
2017-11-15 20:25:33 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -4,64 +4,67 @@ extern crate quickcheck;
 
 use std::mem;
 
-/// Given an array size (`n`), tree layer index (`ipk`) and element index (`li`),
-/// this function computes the index of this value in a sorted array.
-///
-/// This is basically the core of this crate, everything else is trivial.
-/// Also, you usually want to use `PermutationGenerator` instead for convenience.
-///
-/// # How it works
-///
-/// This computes the magic function:
-///
-/// ```text
-/// f(n, k) = 2^floor(log2(n+1)) * 2k  -  max(0, (1+k) * 2^floor(log2(n+1)) - (n + 1))  -  1
-/// (where n ∈ ℕ, k ∈ [0, 1])
-/// ```
-///
-/// Because this is integer math: `k = zk * 2^-ipk`
-/// And because we only care about certain values of zk: `zk = li * 2 + 1`
-///
-/// Even though I discovered this on my own and am quite certain that it's correct,
-/// I only have a very vague feeling about **why** it works. If you want to understand this
-/// (you really don't!), have a look at this sequence:
-///
-/// ```text
-/// a_n = (2n - 2^floor(log2(2n)) + 1) / 2^floor(log2(2n))
-/// (1/2, 1/4, 3/4, 1/8, 3/8, 5/8, 7/8, 1/16, 3/16, 5/16, 7/16, 9/16, 11/16, 13/16, 15/16, ...)
-/// ```
-///
-/// The basic idea is that this sequence basically establishes a mapping between a sorted array
-/// and an eytzinger array: If you look at the plot sideways (literally), you can see the tree
-/// with all its layers. The only secret sauce here is `2^floor(log2(x))` which is elementary to
-/// get exponentially growing windows (to build tree layers).
-pub fn get_permutation_element_by_node(n: usize, ipk: usize, li: usize) -> usize {
-    let zk = li * 2 + 1;
-    // k = zk * 2^-ipk
+/// The basic building blocks this is made of.
+pub mod foundation {
+    /// Given an array size (`n`), tree layer index (`ipk`) and element index (`li`),
+    /// this function computes the index of this value in a sorted array.
+    ///
+    /// This is basically the core of this crate, everything else is trivial.
+    /// Also, you usually want to use `PermutationGenerator` instead for convenience.
+    ///
+    /// # How it works
+    ///
+    /// This computes the magic function:
+    ///
+    /// ```text
+    /// f(n, k) = 2^floor(log2(n+1)) * 2k  -  max(0, (1+k) * 2^floor(log2(n+1)) - (n + 1))  -  1
+    /// (where n ∈ ℕ, k ∈ [0, 1])
+    /// ```
+    ///
+    /// Because this is integer math: `k = zk * 2^-ipk`
+    /// And because we only care about certain values of zk: `zk = li * 2 + 1`
+    ///
+    /// Even though I discovered this on my own and am quite certain that it's correct,
+    /// I only have a very vague feeling about **why** it works. If you want to understand this
+    /// (you really don't!), have a look at this sequence:
+    ///
+    /// ```text
+    /// a_n = (2n - 2^floor(log2(2n)) + 1) / 2^floor(log2(2n))
+    /// (1/2, 1/4, 3/4, 1/8, 3/8, 5/8, 7/8, 1/16, 3/16, 5/16, 7/16, 9/16, 11/16, 13/16, 15/16, ...)
+    /// ```
+    ///
+    /// The basic idea is that this sequence basically establishes a mapping between a sorted array
+    /// and an eytzinger array: If you look at the plot sideways (literally), you can see the tree
+    /// with all its layers. The only secret sauce here is `2^floor(log2(x))` which is elementary to
+    /// get exponentially growing windows (to build tree layers).
+    pub fn get_permutation_element_by_node(n: usize, ipk: usize, li: usize) -> usize {
+        let zk = li * 2 + 1;
+        // k = zk * 2^-ipk
 
-    let last_power_of_two = (n + 2).next_power_of_two() / 2;
+        let last_power_of_two = (n + 2).next_power_of_two() / 2;
 
-    let y = (last_power_of_two >> (ipk - 1)) * zk;
-    let kp = y >> 1;
-    let x = kp + last_power_of_two; // (1+k) * last_power_of_two
-    let x = x.saturating_sub(n + 1);
+        let y = (last_power_of_two >> (ipk - 1)) * zk;
+        let kp = y >> 1;
+        let x = kp + last_power_of_two; // (1+k) * last_power_of_two
+        let x = x.saturating_sub(n + 1);
 
-    //println!("n={} x={} y={} z={} kp={} lpot={}", n, x,y,z, kp, last_power_of_two);
-    y - x - 1
+        //println!("n={} x={} y={} z={} kp={} lpot={}", n, x,y,z, kp, last_power_of_two);
+        y - x - 1
+    }
+
+    pub fn index_to_node(i: usize) -> (usize, usize) {
+        let ipk = (i + 2).next_power_of_two().trailing_zeros() as usize;
+        let li = i + 1 - (1 << (ipk - 1));
+        (ipk, li)
+    }
+
+    pub fn get_permutation_element(n: usize, i: usize) -> usize {
+        let (ipk, li) = index_to_node(i);
+        get_permutation_element_by_node(n, ipk, li)
+    }
 }
 
-pub fn index_to_node(i: usize) -> (usize, usize) {
-    let ipk = (i + 2).next_power_of_two().trailing_zeros() as usize;
-    let li = i + 1 - (1 << (ipk - 1));
-    (ipk, li)
-}
-
-pub fn get_permutation_element(n: usize, i: usize) -> usize {
-    let (ipk, li) = index_to_node(i);
-    get_permutation_element_by_node(n, ipk, li)
-}
-
-
+/*
 pub fn magictest1(from: &[usize], to: &mut [usize]) {
     assert_eq!(from.len(), to.len());
 
@@ -78,15 +81,8 @@ pub fn magictest2(from: &[usize], to: &mut [usize]) {
         *t = from[get_permutation_element(size, i)];
     }
 }
-
-/*
-#[derive(Clone, Copy, Debug)]
-pub struct InplacePermutor;
-
-pub trait Permutor<T> {
-    fn permute(data: &mut [T],
-}
 */
+
 
 fn permute_inplace(data: &mut [usize], permutation: &[usize]) {
     for (i, mut p) in permutation.iter().cloned().enumerate() {
@@ -141,7 +137,7 @@ impl Iterator for PermutationGenerator {
 
         let li = self.li;
         self.li += 1;
-        Some(get_permutation_element_by_node(self.size, self.ipk, li))
+        Some(foundation::get_permutation_element_by_node(self.size, self.ipk, li))
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -164,6 +160,7 @@ pub fn eytzingerize(data: &[usize]) -> Vec<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::foundation::*;
 
     #[test]
     fn magic() {
@@ -260,6 +257,27 @@ mod tests {
             let mut data: Vec<_> = (0..perm.len()).collect();
             permute_inplace(data.as_mut_slice(), perm.as_slice());
             data == perm
+        }
+
+        fn eytzinger_tree_invariants(length: usize) -> bool {
+            let perm: Vec<_> = PermutationGenerator::new(length).collect();
+
+            let mut todo = Vec::new();
+            todo.push((0, 0..length));
+            let mut checked = 0;
+            while let Some((i, range)) = todo.pop() {
+                match perm.get(i) {
+                    Some(&v) => {
+                        if !(range.start <= v && v < range.end) { return false; }
+                        todo.push((2 * (i + 1) - 1, range.start..v));
+                        todo.push((2 * (i + 1), v..range.end));
+                        checked += 1;
+                    }
+                    None => continue,
+                }
+            }
+
+            checked == length
         }
     }
 }
